bf9ed4a75824b4869425da5a05c26bce6459081c
ogundemirkiran <88023745+ogundemirkiran@users.noreply.github.com>
2021-12-01 00:38:51 +0300

    Update index.js

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -11,8 +11,8 @@ function About() {
             Diyet ve Sağlıklı Beslenme
           </Card.Header>
           <Card.Body className="cardText">
-            <Card.Title> Card Title</Card.Title>
-            <Card.Text>Some quick example text to build on the</Card.Text>
+            <Card.Title> Beslenme </Card.Title>
+            <Card.Text>Beslenme ihtiyaçlarınızı karşılamak (tahıllar, meyveler, sebzeler, proteinler, süt ürünleri ve sağlıklı yağlar) tüketmelisiniz.</Card.Text>
           </Card.Body>
         </Card>
         <br />
@@ -20,8 +20,8 @@ function About() {
         <Card className="card card2 " border="secondary">
           <Card.Header>Online Beslenme Danışmanlığı</Card.Header>
           <Card.Body>
-            <Card.Title> Card Title</Card.Title>
-            <Card.Text>Some quick example text to build on the</Card.Text>
+            <Card.Title> Uzaktan Takip </Card.Title>
+            <Card.Text>Bizlere gün içerisinde hızlı bir şekilde ulaşabilir, zamandan tasarruf sağlayabilirsiniz.</Card.Text>
           </Card.Body>
         </Card>
         <br />
@@ -29,8 +29,8 @@ function About() {
         <Card className="card card3 " border="success">
           <Card.Header>Sporcu Beslenmesi</Card.Header>
           <Card.Body>
-            <Card.Title> Card Title</Card.Title>
-            <Card.Text>Some quick example text to build on the</Card.Text>
+            <Card.Title>Sporda Enerji</Card.Title>
+            <Card.Text>Spor esnasında kaslardaki glikojen depolarını dolu tutmak  spor yaparken kas kaybını önler.</Card.Text>
           </Card.Body>
         </Card>
         <br />
@@ -38,8 +38,8 @@ function About() {
         <Card className="card card4 " border="danger">
           <Card.Header>Kurumsal Beslenme Danışmanlığı</Card.Header>
           <Card.Body>
-            <Card.Title> Card Title</Card.Title>
-            <Card.Text>Some quick example text to build on the</Card.Text>
+            <Card.Title>Başarı ve Motivasyon</Card.Title>
+            <Card.Text>Çalışanların sağlıklı yaşamaları için önemli bir yatırımdır.</Card.Text>
           </Card.Body>
         </Card>
 
